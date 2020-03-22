2c96ff68b49a81eb79ac5cf3f832faf457f0cada
Carlos Rivera <carlosrdev@gmail.com>
2020-03-22 17:09:31 -0600

    update

--- src/main/java/com/wtf/medico/controller/UserController.java
+++ src/main/java/com/wtf/medico/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -52,5 +53,16 @@ public class UserController {
 	userRepo.save(theUser);
 	
 	return theUser;
+	}
+	
+	@PutMapping("/users")
+	public User updateUser(@RequestBody User theUser) {
+		Optional<User> tempUser = userRepo.findById(theUser.getUsername());
+		if (tempUser.isPresent()) {
+		userRepo.save(theUser);
+		return theUser;
+		}else {
+			throw new RuntimeException("No se encontro el usuario que desea actualizar");
+		}
 	}
 }
