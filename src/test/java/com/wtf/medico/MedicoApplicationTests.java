8d18a2c58b6dc5db2afd0c8356dc5855535465fb
Carlos Rivera <carlosrdev@gmail.com>
2020-03-21 20:16:42 -0600

    init

---
+++ src/test/java/com/wtf/medico/MedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtf.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
